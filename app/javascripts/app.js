da498c3f7d38bf78c7728b38a450a0c6ea285d59
didil <haritahadil@yahoo.fr>
2017-09-26 17:07:54 +0200

    ui v0

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -2,8 +2,10 @@
 import "../stylesheets/app.css";
 
 // Import libraries we need.
-import { default as Web3} from 'web3';
-import { default as contract } from 'truffle-contract'
+import {default as Web3} from 'web3';
+import {default as contract} from 'truffle-contract'
+import {default as $} from 'jquery';
+
 
 // Import our contract artifacts and turn them into usable abstractions.
 import rpmcoin_artifacts from '../../build/contracts/RPMCoin.json'
@@ -18,14 +20,14 @@ var accounts;
 var account;
 
 window.App = {
-  start: function() {
+  start: function () {
     var self = this;
 
     // Bootstrap the RPMCoin abstraction for Use.
     RPMCoin.setProvider(web3.currentProvider);
 
     // Get the initial account balance so it can be displayed.
-    web3.eth.getAccounts(function(err, accs) {
+    web3.eth.getAccounts(function (err, accs) {
       if (err != null) {
         alert("There was an error fetching your accounts.");
         return;
@@ -40,33 +42,96 @@ window.App = {
       account = accounts[0];
 
       self.refreshBalances();
+      self.refreshVotesToUse();
+      self.refreshUpvotes();
+      self.setAccountAddresses();
     });
   },
 
-  setStatus: function(message) {
+  setStatus: function (message) {
     var status = document.getElementById("status");
     status.innerHTML = message;
   },
 
-  refreshBalances: function() {
+  refreshBalances: function () {
     var self = this;
 
     var RPM;
-    RPMCoin.deployed().then(function(instance) {
+    RPMCoin.deployed().then(function (instance) {
       RPM = instance;
-      return RPM.balanceOf.call(account, {from: account});
-    }).then(function(value) {
-      var balance_element = document.getElementById("balance");
-      balance_element.innerHTML = value.valueOf();
 
-
-    }).catch(function(e) {
+      return Promise.all(
+        [
+          RPM.balanceOf.call(accounts[0], {from: account}),
+          RPM.balanceOf.call(accounts[1], {from: account}),
+          RPM.balanceOf.call(accounts[2], {from: account}),
+          RPM.balanceOf.call(accounts[3], {from: account}),
+          RPM.balanceOf.call(accounts[4], {from: account}),
+        ]
+      );
+    }).then(function (values) {
+      $(".account").each(function (i) {
+        $(this).find('.balance').text(values[i]);
+      });
+    }).catch(function (e) {
       console.log(e);
       self.setStatus("Error getting balances; see log.");
     });
   },
 
-  transfer: function() {
+  refreshVotesToUse: function () {
+    var self = this;
+
+    var RPM;
+    RPMCoin.deployed().then(function (instance) {
+      RPM = instance;
+
+      return Promise.all(
+        [
+          RPM.votesToUse.call(accounts[0], {from: account}),
+          RPM.votesToUse.call(accounts[1], {from: account}),
+          RPM.votesToUse.call(accounts[2], {from: account}),
+          RPM.votesToUse.call(accounts[3], {from: account}),
+          RPM.votesToUse.call(accounts[4], {from: account}),
+        ]
+      );
+    }).then(function (values) {
+      $(".account").each(function (i) {
+        $(this).find('.votes-to-use').text(values[i]);
+      });
+    }).catch(function (e) {
+      console.log(e);
+      self.setStatus("Error getting votesToUse; see log.");
+    });
+  },
+
+  refreshUpvotes: function () {
+    var self = this;
+
+    var RPM;
+    RPMCoin.deployed().then(function (instance) {
+      RPM = instance;
+
+      return Promise.all(
+        [
+          RPM.upvotesReceivedThisWeek.call(accounts[0], {from: account}),
+          RPM.upvotesReceivedThisWeek.call(accounts[1], {from: account}),
+          RPM.upvotesReceivedThisWeek.call(accounts[2], {from: account}),
+          RPM.upvotesReceivedThisWeek.call(accounts[3], {from: account}),
+          RPM.upvotesReceivedThisWeek.call(accounts[4], {from: account}),
+        ]
+      );
+    }).then(function (values) {
+      $(".account").each(function (i) {
+        $(this).find('.upvotes').text(values[i]);
+      });
+    }).catch(function (e) {
+      console.log(e);
+      self.setStatus("Error getting upvotes; see log.");
+    });
+  },
+
+  transfer: function () {
     var self = this;
 
     var amount = parseInt(document.getElementById("amount").value);
@@ -75,27 +140,93 @@ window.App = {
     this.setStatus("Initiating transaction... (please wait)");
 
     var RPM;
-    RPMCoin.deployed().then(function(instance) {
+    RPMCoin.deployed().then(function (instance) {
       RPM = instance;
       return RPM.transferAndAddVoterAddress(receiver, amount, {from: account});
-    }).then(function() {
+    }).then(function () {
       self.setStatus("Transaction complete!");
       self.refreshBalances();
-    }).catch(function(e) {
+    }).catch(function (e) {
       console.log(e);
       self.setStatus("Error sending coin; see log.");
+    });
+  },
+
+  distributeVotes: function () {
+    var self = this;
+
+    var amount = parseInt(document.getElementById("votes-to-distribute").value);
+
+    this.setStatus("Distributing votes ... (please wait)");
+
+    var RPM;
+    RPMCoin.deployed().then(function (instance) {
+      RPM = instance;
+      return RPM.distributeVotes(amount, {from: account,gas: 900000});
+    }).then(function () {
+      self.setStatus("Transaction complete!");
+      self.refreshVotesToUse();
+    }).catch(function (e) {
+      console.log(e);
+      self.setStatus("Error distributing votes; see log.");
+    });
+  },
+
+  distributeTokens: function () {
+    var self = this;
+
+    var amount = parseInt(document.getElementById("tokens-to-distribute").value);
+
+    this.setStatus("Distributing tokens ... (please wait)");
+
+    var RPM;
+    RPMCoin.deployed().then(function (instance) {
+      RPM = instance;
+      return RPM.distributeTokens(amount, {from: account,gas: 900000});
+    }).then(function () {
+      self.setStatus("Transaction complete!");
+      self.refreshBalances();
+    }).catch(function (e) {
+      console.log(e);
+      self.setStatus("Error distributing tokens; see log.");
+    });
+  },
+
+  vote: function (i) {
+    var self = this;
+
+    this.setStatus("Voting ... (please wait)");
+
+    var RPM;
+    RPMCoin.deployed().then(function (instance) {
+      RPM = instance;
+      return RPM.vote(account, accounts[i] , {from: account,gas: 900000});
+    }).then(function () {
+      self.setStatus("Transaction complete!");
+      self.refreshUpvotes();
+      self.refreshVotesToUse();
+    }).catch(function (e) {
+      console.log(e);
+      self.setStatus("Error distributing tokens; see log.");
+    });
+  },
+
+  setAccountAddresses: function () {
+    $(".account").each(function (i) {
+      $(this).find('.address').text(accounts[i]);
     });
   }
 };
 
-window.addEventListener('load', function() {
-  // Checking if Web3 has been injected by the browser (Mist/RPMMask)
+
+$(function () {
+  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
   if (typeof web3 !== 'undefined') {
-    console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 RPMCoin, ensure you've configured that source properly. If using RPMMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-RPMmask")
-    // Use Mist/RPMMask's provider
+    console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 RPMCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-MetaMask")
+    // Use Mist/MetaMask's provider
     window.web3 = new Web3(web3.currentProvider);
   } else {
-    console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to RPMmask for development. More info here: http://truffleframework.com/tutorials/truffle-and-RPMmask");
+    console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to MetaMask for development. More info here: http://truffleframework.com/tutorials/truffle-and-MetaMask");
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
     window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
   }
